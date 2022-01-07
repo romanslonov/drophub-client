f8531d7f76fca7afc3ccf0cd85476570423e1a66
Roman <destabiliseus@gmail.com>
2022-01-07 17:02:10 +0800

    Added Textbox and Button

---
+++ src/components/Button.vue
@@ -0,0 +1,26 @@
+<template>
+  <button
+    class="block font-medium text-sm rounded h-10 px-4"
+    :class="{
+      'bg-blue-600 hover:bg-blue-700 text-white': appearance === 'primary',
+      'border-2 bg-white hover:border-gray-300 hover:bg-gray-100 text-gray-900':
+        appearance === 'default',
+    }"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Button',
+  props: {
+    appearance: {
+      type: String,
+      default: 'default',
+    },
+  },
+});
+</script>
---
+++ src/components/Textbox.vue
@@ -0,0 +1,49 @@
+<template>
+  <textarea v-if="multiline" name="" id="" cols="30" rows="10"></textarea>
+  <div v-else>
+    <label :for="name" class="block text-sm font-medium text-gray-700 mb-1">{{ label }}</label>
+    <input
+      :name="name"
+      :id="name"
+      :placeholder="placeholder"
+      :type="type"
+      class="block w-full border-2 text-sm hover:border-gray-300 rounded-md h-10 px-2"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  emits: ['update:modelValue'],
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      default: 'text',
+    },
+    multiline: {
+      type: Boolean,
+      default: false,
+    },
+    modelValue: {
+      type: String,
+      required: true,
+    },
+    placeholder: {
+      type: String,
+      default: '',
+    },
+  },
+});
+</script>
